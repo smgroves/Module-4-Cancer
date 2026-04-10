2b7149172579c4e9e461abcea1cf23268de18984
TomasDaniel1 <tomasdan64@gmail.com>
2026-04-10 19:00:26 -0400

    Template + Genes csv file

--- code/example_EDA.py
+++ code/example_EDA.py
@@ -8,9 +8,9 @@ import seaborn as sns
 # Load the data
 ####################################################
 data = pd.read_csv(
-    '../Data/TRAINING_SET_GSE62944_subsample_log2TPM.csv', index_col=0, header=0)  # can also use larger dataset with more genes
+    '/Users/tomas/CompBME/Module4_Vikesland+Daniel/Module-4-Cancer/data/TRAINING_SET_GSE62944_subsample_log2TPM.csv', index_col=0, header=0)  # can also use larger dataset with more genes
 metadata_df = pd.read_csv(
-    '../Data/TRAINING_SET_GSE62944_metadata.csv', index_col=0, header=0)
+    '/Users/tomas/CompBME/Module4_Vikesland+Daniel/Module-4-Cancer/data/TRAINING_SET_GSE62944_metadata.csv', index_col=0, header=0)
 print(data.head())
 
 # %%
@@ -29,7 +29,7 @@ print(metadata_df.describe())
 # %%
 # Subset the data for a specific cancer type
 ####################################################
-cancer_type = 'BRCA'  # Breast Invasive Carcinoma
+cancer_type = 'LUAD'  # Breast Invasive Carcinoma
 
 # From metadata, get the rows where "cancer_type" is equal to the specified cancer type
 # Then grab the index of this subset (these are the sample IDs)
@@ -37,27 +37,27 @@ cancer_samples = metadata_df[metadata_df['cancer_type'] == cancer_type].index
 print(cancer_samples)
 # Subset the main data to include only these samples
 # When you want a subset of columns, you can pass a list of column names to the data frame in []
-BRCA_data = data[cancer_samples]
+LUAD_data = data[cancer_samples]
 
 # %%
 # Subset by index (genes)
 ####################################################
 desired_gene_list = ['TP53', 'BRCA1', 'BRCA2', 'EGFR', 'MYC']
-gene_list = [gene for gene in desired_gene_list if gene in BRCA_data.index]
+gene_list = [gene for gene in desired_gene_list if gene in LUAD_data.index]
 for gene in desired_gene_list:
     if gene not in gene_list:
         print(f"Warning: {gene} not found in the dataset.")
 
 # .loc[] is the method to subset by index labels
 # .iloc[] will subset by index position (integer location) instead
-BRCA_gene_data = BRCA_data.loc[gene_list]
-print(BRCA_gene_data.head())
+BRCA_gene_data = LUAD_data.loc[gene_list]
+print(LUAD_data.head())
 
 # %%
 # Basic statistics on the subsetted data
 ####################################################
-print(BRCA_gene_data.describe())
-print(BRCA_gene_data.var(axis=1))  # Variance of each gene across samples
+print(LUAD_data.describe())
+print(LUAD_data.var(axis=1))  # Variance of each gene across samples
 # Mean expression of each gene across samples
 print(BRCA_gene_data.mean(axis=1))
 # Median expression of each gene across samples
